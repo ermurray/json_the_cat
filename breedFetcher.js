ce811fdb6ea632c3dfd8c0355c72f0b6d55fe1c8
ermurray <ermurray31@gmail.com>
2021-02-11 21:32:34 +0000

    added logic to deal with edge case of error in file path

--- breedFetcher.js
+++ breedFetcher.js
@@ -8,13 +8,14 @@ const query = `${url}?q=${breed}`;
 request(query, (error, response, body) => {
   if (error) {
     return console.log('not valid url');
+  } else if (response.statusCode === 404) { // incase typo is not domain but in the file path.
+    return console.log('not valid file path');
   }
   const data = JSON.parse(body);
-  console.log(error);
+  console.log(response.statusCode);
   if (data[0] === undefined) {
     console.log(`No such breed of ${breed} found`);
   } else {
     console.log(data[0].description);
   }
-  
 });
