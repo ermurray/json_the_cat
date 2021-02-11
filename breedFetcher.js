47b705ced1c2bbf7381abf6d0397719a912dfd88
ermurray <ermurray31@gmail.com>
2021-02-11 23:12:20 +0000

    refactored and moved request logic to fetchBreedDescription function and  all console logs to index.js exported fetchbreed function to be used by index

--- breedFetcher.js
+++ breedFetcher.js
@@ -1,21 +1,23 @@
 const request = require('request');
 
-const url = 'https://api.thecatapi.com/v1/breeds/search';
-const breed = process.argv[2];
-const query = `${url}?q=${breed}`;
 
+const fetchBreedDescription = function(breedName, callback) {
+  const url = 'https://api.thecatapi.com/v1/breeds/search';
+  const query = `${url}?q=${breedName}`;
+  request(query, (error, response, body) => {
+    if (error) {
+      return callback(error, null);
+    } else if (response.statusCode === 404) { // incase typo is not domain but in the file path.
+      return callback(response.statusCode, null);
+    }
+    const data = JSON.parse(body);
+    if (data[0] === undefined) {
+      return callback(`No such breed of ${breedName} found`, null);
+    } else {
+      return callback(null, data[0].description);
+    }
+  });
+ 
+};
 
-request(query, (error, response, body) => {
-  if (error) {
-    return console.log('not valid url');
-  } else if (response.statusCode === 404) { // incase typo is not domain but in the file path.
-    return console.log('not valid file path');
-  }
-  const data = JSON.parse(body);
-  console.log(response.statusCode);
-  if (data[0] === undefined) {
-    console.log(`No such breed of ${breed} found`);
-  } else {
-    console.log(data[0].description);
-  }
-});
+module.exports = { fetchBreedDescription };
